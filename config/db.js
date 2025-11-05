62e8ca2d47b48e099b6a34d9414b66e7709023fa
Milton Berrocal <miltonberrocal90@gmail.com>
2025-11-05 14:25:58 -0500

    Corrige conexión a MongoDB Atlas

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('✅ Conectado a MongoDB');
   } catch (err) {
     console.error('❌ Error al conectar a MongoDB:', err.message);
